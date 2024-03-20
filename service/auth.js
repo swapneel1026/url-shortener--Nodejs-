4d46e5d760c6f5f442b4f2485623705a96e7397f
swapneel1026 <swapneelshubham44@gmail.com>
2024-03-20 17:59:49 +0530

    add expiry time in jwt

--- service/auth.js
+++ service/auth.js
@@ -1,12 +1,12 @@
 import jwt from "jsonwebtoken";
-
 const secret = "abcdefghi$@#";
+
 export function setUser(user) {
   const payload = {
     id: user._id,
     email: user.email,
   };
-  return jwt.sign(payload, secret);
+  return jwt.sign(payload, secret, { expiresIn: "10d" });
 }
 export function getUser(token) {
   return jwt.verify(token, secret);
